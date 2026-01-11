a269fa531feaa7be0b92a361ce0f7b7bfd0e910f
kid141252010 <kid@nanhaijue1412.onmicrosoft.com>
2026-01-11 22:49:54 +0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -8,7 +8,7 @@ import fetchJsonp from "fetch-jsonp";
 // 获取音乐播放列表
 export const getPlayerList = async (server, type, id) => {
   const res = await fetch(
-    `${import.meta.env.VITE_SONG_API}?server=${server}&type=${type}&id=${id}`,
+    `${import.meta.env.VITE_SONG_API}v1?id=${id}&level=sq`,
   );
   const data = await res.json();
 
